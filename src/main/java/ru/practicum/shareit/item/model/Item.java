d0e0c0caa5550b1a278f2ec4f648ae7b8cb8e378
Alex Savelev <ryan.hollywood@yandex.ru>
2022-07-25 17:23:55 +0300

    refactor: item and user added

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,30 @@
 package ru.practicum.shareit.item.model;
 
+import lombok.Data;
+import ru.practicum.shareit.requests.ItemRequest;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 /**
  * // TODO .
  */
+
+@Data
 public class Item {
+    long id;
+
+    @NotNull
+    @NotBlank
+    String name;
+
+    @NotNull
+    @NotBlank
+    String description;
+
+    @NotNull
+    Boolean available;
+
+    long ownerId;
+    ItemRequest request;
 }
--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,7 +1,27 @@
 package ru.practicum.shareit.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 /**
  * // TODO .
  */
+
+@Data
+@AllArgsConstructor
 public class User {
+    @NotNull
+    long id;
+
+    @NotNull
+    @NotBlank
+    String name;
+
+    @NotNull
+    @Email
+    String email;
 }
