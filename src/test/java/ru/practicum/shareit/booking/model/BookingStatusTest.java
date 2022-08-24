94145e3e08bd83130932c8d9c02bd1af36c2daca
Alex Savelev <ryan.hollywood@yandex.ru>
2022-08-24 11:09:10 +0300

    test: bookingStatus test is ready

--- src/test/java/ru/practicum/shareit/booking/model/BookingStatusTest.java
+++ src/test/java/ru/practicum/shareit/booking/model/BookingStatusTest.java
@@ -2,15 +2,20 @@ package ru.practicum.shareit.booking.model;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BookingStatusTest {
 
-    @Test
-    void values() {
-    }
+    private final String[] STATUS_VALUES = new String[] {"WAITING", "APPROVED", "REJECTED", "CANCELLED"};
 
     @Test
-    void valueOf() {
+    void values() {
+        int counter = 0;
+        for (BookingStatus status : BookingStatus.values()) {
+            assertEquals(STATUS_VALUES[counter], String.valueOf(status));
+            counter++;
+        }
     }
 }
