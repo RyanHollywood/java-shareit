e8aaa32a6903153e418f77fa7d4049c4a936f3b2
Alex Savelev <ryan.hollywood@yandex.ru>
2022-08-24 12:48:17 +0300

    refactor: ItemRequestController with endpoints, empty ItemRequestService added

--- src/main/java/ru/practicum/shareit/requests/ItemRequestController.java
+++ src/main/java/ru/practicum/shareit/requests/ItemRequestController.java
@@ -1,9 +1,36 @@
 package ru.practicum.shareit.requests;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.requests.dto.ItemRequestDto;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/requests")
 public class ItemRequestController {
+
+    @PostMapping
+    public ItemRequestDto create() {
+        return null;
+    }
+
+    @PatchMapping("/{requestId}")
+    public ItemRequestDto update() {
+        return null;
+    }
+
+    @GetMapping("/{requestId}")
+    public ItemRequestDto get() {
+        return null;
+    }
+
+    @GetMapping
+    public List<ItemRequestDto> getAll() {
+        return null;
+    }
+
+    @DeleteMapping("/{requestId}")
+    public void delete() {
+
+    }
 }
--- src/test/java/ru/practicum/shareit/booking/BookingMapperTest.java
+++ src/test/java/ru/practicum/shareit/booking/BookingMapperTest.java
@@ -1,13 +1,42 @@
 package ru.practicum.shareit.booking;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import ru.practicum.shareit.booking.dto.BookingDto;
+import ru.practicum.shareit.booking.dto.BookingItemDto;
+import ru.practicum.shareit.booking.dto.BookingRequestDto;
+import ru.practicum.shareit.booking.model.Booking;
+import ru.practicum.shareit.booking.model.BookingStatus;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.model.User;
+
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BookingMapperTest {
 
+    private final long ID = 1;
+    private final LocalDateTime START = LocalDateTime.now();
+    private final LocalDateTime END = LocalDateTime.now().plusHours(1);
+    private final Item ITEM = null;
+    private final User BOOKER = null;
+    private final BookingStatus DEFAULT_STATUS = BookingStatus.WAITING;
+    private Booking booking;
+    private BookingDto bookingDto;
+    private BookingRequestDto bookingRequestDto;
+    private BookingItemDto bookingItemDto;
+
+    @BeforeEach
+    void reload() {
+        booking = new Booking(START, END, null, null, DEFAULT_STATUS);
+        booking.setId(ID);
+        bookingDto = new BookingDto(ID, START, END, null, null, DEFAULT_STATUS);
+    }
+
     @Test
     void toBookingDto() {
+        assertEquals(bookingDto, BookingMapper.toBookingDto(booking));
     }
 
     @Test
