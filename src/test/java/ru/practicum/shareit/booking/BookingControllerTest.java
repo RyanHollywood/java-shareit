d32a6bbdfbce6aac76d8132fefcc0fd5aa0cc79a
Alex Savelev <ryan.hollywood@yandex.ru>
2022-09-01 15:55:48 +0300

    test: booking service test - not ready yet, commit just for GitAction coverage test

--- src/test/java/ru/practicum/shareit/booking/BookingControllerTest.java
+++ src/test/java/ru/practicum/shareit/booking/BookingControllerTest.java
@@ -1,16 +1,31 @@
 package ru.practicum.shareit.booking;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import ru.practicum.shareit.booking.service.BookingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.practicum.shareit.booking.service.BookingServiceImpl;
 
 @ExtendWith(MockitoExtension.class)
 class BookingControllerTest {
 
     @Mock
-    private BookingService bookingService;
+    private BookingServiceImpl bookingService;
+
+    @InjectMocks
+    private BookingController bookingController;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void reload() {
+
+    }
 
     @Test
     void create() {
--- src/test/java/ru/practicum/shareit/booking/service/BookingServiceImplTest.java
+++ src/test/java/ru/practicum/shareit/booking/service/BookingServiceImplTest.java
@@ -1,13 +1,65 @@
 package ru.practicum.shareit.booking.service;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.practicum.shareit.booking.BookingMapper;
+import ru.practicum.shareit.booking.dto.BookingRequestDto;
+import ru.practicum.shareit.booking.model.Booking;
+import ru.practicum.shareit.booking.model.BookingStatus;
+import ru.practicum.shareit.booking.storage.BookingRepository;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.item.storage.ItemRepository;
+import ru.practicum.shareit.user.model.User;
+import ru.practicum.shareit.user.storage.UserRepository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDateTime;
+import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class BookingServiceImplTest {
 
+    @Mock
+    private BookingRepository bookingRepository;
+
+    @Mock
+    private ItemRepository itemRepository;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private BookingServiceImpl bookingService;
+    private Booking booking;
+    private BookingRequestDto bookingRequestDto;
+    private Item item;
+    private User user;
+
+    @BeforeEach
+    void reload() {
+        bookingRequestDto = new BookingRequestDto(1, 1, LocalDateTime.now().plusMinutes(30), LocalDateTime.now().plusHours(1));
+        item = new Item(1, "Item", "ItemDescription", true, 1, 1);
+        user = new User(2, "User", "Email@email.com");
+        booking = new Booking(LocalDateTime.now().plusMinutes(30), LocalDateTime.now().plusHours(1), item, user, BookingStatus.WAITING);
+        booking.setId(1);
+    }
+
     @Test
-    void create() {
+    void create() throws Exception {
+        checkItem();
+        checkUser();
+        when(bookingRepository.save(any()))
+                .thenReturn(booking);
+        assertEquals(BookingMapper.toBookingDto(booking), bookingService.create(bookingRequestDto));
+
     }
 
     @Test
@@ -16,6 +68,9 @@ class BookingServiceImplTest {
 
     @Test
     void getById() {
+        checkItem();
+        checkBooking();
+        assertEquals(BookingMapper.toBookingDto(booking), bookingService.getById(1, 1));
     }
 
     @Test
@@ -28,5 +83,20 @@ class BookingServiceImplTest {
 
     @Test
     void delete() {
+    }
+
+    private void checkItem() {
+        when(itemRepository.findById(anyLong()))
+                .thenReturn(Optional.of(item));
+    }
+
+    private void checkUser() {
+        when(userRepository.findById(anyLong()))
+                .thenReturn(Optional.of(user));
+    }
+
+    private void checkBooking() {
+        when(bookingRepository.findById(anyLong()))
+                .thenReturn(Optional.of(booking));
     }
 }
