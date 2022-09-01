1ed3ce91fa8a488ff6001fd4c94491c7d4ad3227
Alex Savelev <ryan.hollywood@yandex.ru>
2022-09-01 16:10:08 +0300

    test: exception package test files added

---
+++ src/test/java/ru/practicum/shareit/exceptions/ErrorControllerTest.java
@@ -0,0 +1,24 @@
+package ru.practicum.shareit.exceptions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ErrorControllerTest {
+
+    @Test
+    void handleIncorrectParameterException() {
+    }
+
+    @Test
+    void testHandleIncorrectParameterException() {
+    }
+
+    @Test
+    void testHandleIncorrectParameterException1() {
+    }
+
+    @Test
+    void testHandleIncorrectParameterException2() {
+    }
+}
---
+++ src/test/java/ru/practicum/shareit/exceptions/errors/ConflictTest.java
@@ -0,0 +1,7 @@
+package ru.practicum.shareit.exceptions.errors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConflictTest {
+
+}
