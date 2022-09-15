d0856188d6fe6f844acc50f024648ecfec8dc297
Alex Savelev <ryan.hollywood@yandex.ru>
2022-09-15 19:04:58 +0300

    refactor: shareIt-server module added

--- src/main/java/ru/practicum/shareit/exceptions/errors/InternalServerError.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exceptions.errors;
-
-public class InternalServerError extends RuntimeException {
-    public InternalServerError(String msg) {
-        super(msg);
-    }
-}
--- src/main/java/ru/practicum/shareit/exceptions/errors/NotFound.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exceptions.errors;
-
-public class NotFound extends RuntimeException {
-    public NotFound(String msg) {
-        super(msg);
-    }
-}
