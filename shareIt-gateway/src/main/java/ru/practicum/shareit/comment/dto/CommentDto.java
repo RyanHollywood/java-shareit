2722ed5f027e2512c34fe27532f47a9e7dc63e9d
Alex Savelev <ryan.hollywood@yandex.ru>
2022-09-18 18:50:41 +0000

    fix: NotEmpty annotation to commentDto in gateway

--- shareIt-gateway/src/main/java/ru/practicum/shareit/comment/dto/CommentDto.java
+++ shareIt-gateway/src/main/java/ru/practicum/shareit/comment/dto/CommentDto.java
@@ -3,11 +3,15 @@ package ru.practicum.shareit.comment.dto;
 import lombok.Data;
 import ru.practicum.shareit.item.dto.ItemDto;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Data
 public class CommentDto {
     private long id;
+    @NotEmpty
+    @NotNull
     private String text;
     private ItemDto itemDto;
     private String authorName;
