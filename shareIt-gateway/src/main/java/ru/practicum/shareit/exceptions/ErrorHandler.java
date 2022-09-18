54230466850d1d8326df5e24d1fe862e6fb3a507
Alex Savelev <ryan.hollywood@yandex.ru>
2022-09-18 19:58:46 +0000

    fix: exception handler hotfix

--- shareIt-gateway/src/main/java/ru/practicum/shareit/exceptions/ErrorHandler.java
+++ shareIt-gateway/src/main/java/ru/practicum/shareit/exceptions/ErrorHandler.java
@@ -2,41 +2,35 @@ package ru.practicum.shareit.exceptions;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import ru.practicum.shareit.exceptions.errors.BadRequestException;
 import ru.practicum.shareit.exceptions.errors.Conflict;
 import ru.practicum.shareit.exceptions.errors.InternalServerError;
 import ru.practicum.shareit.exceptions.errors.NotFound;
-
-import java.util.Map;
 
 @ControllerAdvice
 @Slf4j
 public class ErrorHandler {
 
     @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleIncorrectParameterException(final BadRequestException exception) {
-        return exception.getMessage();
+    public ResponseEntity<?> handle(final BadRequestException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handleIncorrectParameterException(final NotFound exception) {
-        return exception.getMessage();
+    public ResponseEntity<?> handle(final NotFound exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler
-    @ResponseStatus(HttpStatus.CONFLICT)
-    public String handleIncorrectParameterException(final Conflict exception) {
-        return exception.getMessage();
+    public ResponseEntity<?> handle(final Conflict exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public Map handleIncorrectParameterException(final InternalServerError exception) {
-        return Map.of("error", exception.getMessage());
+    public ResponseEntity<?> handle(final InternalServerError exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
